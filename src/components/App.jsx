76c3d5fd90b0561bc8f5c0396d40fdedc5687986
prashant1682 <prashant009@gmail.com>
2020-12-31 14:26:07 +0530

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [headingName, setHeadingName] = useState("");
+
+  function handleOnChnage(event) {
+    setName(event.target.value);
+  }
+
+  function handleOnClick(event) {
+    setHeadingName(name);
+
+    //The below code prevet the form below to be refreshed and removing the name which was rendered
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <form>
+        <h1>Hello {headingName}</h1>
+        <input
+          onChange={handleOnChnage}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button onClick={handleOnClick}>Submit</button>
+      </form>
     </div>
   );
 }
